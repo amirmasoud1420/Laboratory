021b1181a72b82c883d245227260b2b443033aa7
unknown <amir.talebnia@gmail.com>
2021-05-31 18:46:01 +0430

    compelete save to file

--- lab/menu_veiws.py
+++ lab/menu_veiws.py
@@ -19,23 +19,20 @@ def corona_test_register():
     print("you successfully registered...")
     d_date = date(year=date.today().year, month=date.today().month, day=date.today().day)
     r_date = date(year=date.today().year, month=date.today().month, day=date.today().day)
-    print("please come to labratoary for ddoing test in : ", d_date)
+    print("please come to laboratory for doing test in : ", d_date)
     print("your result is ready in : ", r_date)
-    c_test = CoronaTest(p, d_date, r_date,result)
-    with open(f"files\corona_tests\{p.national_code}.txt", 'wb') as f:
+    c_test = CoronaTest(p, d_date, r_date, result)
+    with open(f"files/patients/{p.national_code}.txt", 'wb') as f:
         dill.dump(p, f)
-    with open(f"files\corona_tests\{p.national_code}.txt", 'wb') as f:
+    with open(f"files/corona_tests/{p.national_code}.txt", 'wb') as f:
         dill.dump(c_test, f)
 
 
 def corona_test_view_result():
     n_code = input("pleas enter your national code : ")
-    if os.path.exists(f"files\corona_tests\{n_code}.txt"):
-        with open(f"files\corona_tests\{n_code}.txt", 'rb') as f:
+    if os.path.exists(f"files/corona_tests/{n_code}.txt"):
+        with open(f"files/corona_tests/{n_code}.txt", 'rb') as f:
             c_test = dill.load(f)
         print('your result is : ', c_test.result)
     else:
         print("your national code is not exist!!!")
-
-
-
